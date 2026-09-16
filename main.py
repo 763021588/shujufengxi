8925ce46ec42077bec3b44a533c151e501822152
763021588 <763021588@qq.com>
2026-09-16 11:37:13 +0800

    图片生成

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from datetime import datetime
+import matplotlib.pyplot as plt
 import json
+
+from pandas import pivot
+
 
 class 数据处理():
 
@@ -42,8 +46,8 @@ class 数据处理():
                                      出现次数=('备注','count')))
 
     def 月份统计(self):
-        self.df['日期'] = self.df['日期'].astype(str)
         x=self.df.copy()
+        x['日期'] = self.df['日期'].astype(str)
         x['日期']=self.df['日期'].str[:6]
         shou=x.loc[x['类型']=='收入']
         zhi=x.loc[x['类型']=='支出']
@@ -103,9 +107,75 @@ class 数据处理():
             return
         print('修改成功')
         print(self.df)
+class image():
+    def __init__(self,文件=r'C:\Users\wei\PyCharmMiscProject\zhangben.csv'):
+        self.文件 = 文件
+        try:
+            if 文件.endswith('.csv'):
+                self.df = pd.read_csv(文件)
+            elif 文件.endswith('.json'):
+                self.df = pd.read_json(文件)
+            else:
+                print('不支持的文件')
+        except FileNotFoundError:
+            print('找不到文件')
+            self.df = pd.DataFrame()
+        except Exception as e:
+            print(e)
+
+    def bar_chart(self):
+        income=self.df.loc[self.df['类型']=='收入']
+        income=income.groupby('备注')['金额'].sum()
+        spending=self.df.loc[self.df['类型']=='支出']
+        spending=spending.groupby('备注')['金额'].sum()
+        fig, ax = plt.subplots(1,2)
+        x=income.index
+        y=income.values
+        x1=spending.index
+        y1=spending.values
+        plt.rcParams["font.sans-serif"] = [
+            "Microsoft YaHei"]
+        plt.rcParams["axes.unicode_minus"] = False
+        ax[0].bar(x,y,alpha=0.8)
+        ax[0].set_title('各备注收入汇总')
+        ax[0].set_xlabel('备注')
+        ax[0].set_ylabel('金额')
+        ax[1].bar(x1,y1,alpha=0.8)
+        ax[1].set_title('各备注支出汇总')
+        ax[1].set_xlabel('备注')
+        ax[1].set_ylabel('金额')
+        fig.savefig("备注.png", dpi=300, bbox_inches="tight")
+        plt.show()
+
+    def Line_chart(self):
+        shou=self.df.loc[self.df['类型']=='收入'].groupby('日期')['金额'].sum()
+        zhi=self.df.loc[self.df['类型']=='支出'].groupby('日期')['金额'].sum()
+        net_income=shou.sub(zhi,fill_value=0)
+        plt.rcParams["font.sans-serif"] = [
+            "Microsoft YaHei"]
+        plt.rcParams["axes.unicode_minus"] = False
+        plt.plot(net_income.index,net_income)
+        plt.xticks(net_income.index)
+        plt.xlabel('日期')
+        plt.title('月金额变化')
+        plt.ylabel('金额')
+        plt.savefig("月折线图.png", dpi=300, bbox_inches="tight")
+        plt.show()
+
+    def 用pandas画图试试(self):
+        plt.rcParams["font.sans-serif"] = [
+            "Microsoft YaHei"]
+        plt.rcParams["axes.unicode_minus"] = False
+        pi=self.df.groupby(['备注','类型'])['金额'].sum().unstack('类型')
+        pi.plot(kind='bar',title='备注分析')
+        plt.tight_layout()
+        plt.savefig("备注汇总.png", dpi=300, bbox_inches="tight")
+        plt.show()
+
 
 if __name__ == '__main__':
     cl = 数据处理()
+    im = image()
     while True:
         xuan = input('按数字选择功能')
         xuan = xuan.replace(' ', '').strip()
@@ -148,4 +218,7 @@ if __name__ == '__main__':
             cl.日期查询(日期)
             continue
         if xuan == 7:
+            im.用pandas画图试试()
+            im.Line_chart()
+            im.bar_chart()
             break
